c61dd878d5e9ace90a975ba45a8aaf7a7150159a
José Ribeiro <me@joseribeiro.dev>
2024-04-24 15:28:22 -0300

    fix specs assertions title

--- spec/lib/field/text_spec.rb
+++ spec/lib/field/text_spec.rb
@@ -2,7 +2,7 @@
 
 RSpec.describe Bleuprint::Field::Text, type: :model do
   describe ".input_type" do
-    it "returns :switch as the input type" do
+    it "returns :input as the input type" do
       expect(described_class.input_type).to eq(:input)
     end
   end
