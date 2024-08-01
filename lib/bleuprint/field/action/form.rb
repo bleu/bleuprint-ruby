fdcd8852e7c26dc43e68dc1485f04452cc7b32f1
devjoaov <joaovictorass95@gmail.com>
2024-08-01 14:47:04 -0300

    fix: fix rubocop erros

--- lib/bleuprint/field/action/form.rb
+++ lib/bleuprint/field/action/form.rb
@@ -15,14 +15,26 @@ module Bleuprint
         def as_json
           {
             name: label,
-            type: type.to_s.gsub("action_", "").to_sym,
+            type: action_type,
             method: options[:method],
             trigger_confirmation: options[:trigger_confirmation],
             condition_key: options[:condition_key],
             condition_value: options[:condition_value],
             url_path: value,
-            form: options[:form_fields].present? ? ::Bleuprint::Forms::Action.new(resource, dashboard, options[:form_fields]).call! : {}
+            form: form_fields_json
           }
+        end
+
+        def action_type
+          type.to_s.gsub("action_", "").to_sym
+        end
+
+        def form_fields_json
+          if options[:form_fields].present?
+            ::Bleuprint::Forms::Action.new(resource, dashboard, options[:form_fields]).call!
+          else
+            {}
+          end
         end
       end
     end
